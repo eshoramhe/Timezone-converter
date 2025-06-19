0631c24450027f1fcd5411b3cec008b74f6afab1
eshoramhe <eshoramhe@yahoo.com>
2025-06-19 21:34:32 +0200

    timezone_converter.py

--- timezone_converter.py
+++ timezone_converter.py
@@ -15,13 +15,25 @@ st.write(
 st.header("1. Enter Date and Time")
 col1, col2 = st.columns(2)
 with col1:
-    date_input = st.date_input("Select a date", datetime.date.today())
+    date_input = st.date_input("Select a date", value=datetime.date.today())
+    # Keeping default date as today, but user can change it.
+
 with col2:
+    # Set value to None to make it appear empty initially.
+    # The widget will still guide the user towards a valid time format.
     time_input = st.time_input("Select a time (HH:MM)", value=None)
 
+# --- Handling potential None value for time_input ---
+# If the user hasn't selected a time yet, time_input will be None.
+# We need to provide a default time (e.g., midnight) or wait for user input.
+if time_input is None:
+    st.warning("Please enter a time.")
+    # Stop the execution until a time is entered.
+    # Alternatively, you could default to a specific time (e.g., datetime.time(0,0))
+    # if you prefer the app to always show a result.
+    st.stop()
+
 # Combine date and time
-# It's good practice to ensure the user has actually selected something before proceeding,
-# though the widgets always return a value.
 combined_datetime = datetime.datetime.combine(date_input, time_input)
 
 st.write(f"You selected: **{combined_datetime.strftime('%Y-%m-%d %H:%M:%S')}**")
@@ -54,7 +66,7 @@ st.header("4. Converted Time")
 st.success(
     f"**{localized_datetime.strftime('%Y-%m-%d %H:%M:%S %Z%z')}** "
     f"in **{source_timezone_str}** is "
-    f"**{converted_datetime.strftime('%Y-%m-%d %H:%M:%S %Z%z')}** "
+    f"**{converted_datetime.strftime('%Y-%m-%d %H:%MM:%S %Z%z')}** "
     f"in **{target_timezone_str}**."
 )
 
